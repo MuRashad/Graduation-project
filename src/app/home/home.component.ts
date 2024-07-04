696323f66021ac0d47b28198ce76d04f35b6f2b1
Your Name <you@example.com>
2024-07-04 07:14:31 +0300

    done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,17 @@ import { Component, ElementRef, ViewChild, AfterViewInit, Renderer2 } from '@ang
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {}
+
+  scrollToContact(event: Event): void {
+    event.preventDefault(); // Prevent default action (e.g., navigation)
+
+    const contactSection = document.getElementById('contactSection');
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 
 }
